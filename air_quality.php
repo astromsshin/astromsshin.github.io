2ec449cec9d5fea28135733dce9a43b6dbc8e054
Min-Su Shin <msshin@kasi.re.kr>
2016-06-18 14:07:55 +0900

    deleted:    air_quality.php

--- air_quality.php
+++
@@ -1,7 +0,0 @@
-<?php
-$str1 = date('Y-m-d', time() - (24*60*60));
-$str2 = date('Y-m-d', time());
-$str3 = date('H', time() - (60*60));
-$str4 = date('H', time());
-header('Location: http://www.airkorea.or.kr/realSearch_pop?stationCode=525141&period=undefined&dateDiv=1&from_date='.$str1.'&to_date='.$str2.'&from_date_hour='.$str3.'&to_date_hour='.$str4);
-?>
